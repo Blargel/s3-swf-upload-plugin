6000ea08bc4e2c7f8bee68b60b4856191c0e7bc5
Abner Qian <LargeBagel@gmail.com>
2010-11-15 20:09:30 -0800

    Got rid of unnecessary if else construction.

--- lib/s3_swf_upload/s3_config.rb
+++ lib/s3_swf_upload/s3_config.rb
@@ -6,42 +6,28 @@ module S3SwfUpload
     cattr_accessor :bucket, :max_file_size, :acl
 
     def self.load_config
-      if ENV['RACK_ENV'] = 'production'
-        @@access_key_id     = ENV['AMAZON_ACCESS_KEY_ID']
-        @@secret_access_key = ENV['AMAZON_SECRET_ACCESS_KEY']
-        @@bucket            = ENV['AMAZON_S3_SWF_UPLOAD_BUCKET']
-        @@max_file_size     = ENV['AMAZON_S3_SWF_MAX_FILE_SIZE']  || 10485760
-        @@acl               = ENV['AMAZON_S3_SWF_UPLOAD_ACL']     || 'private'
-        
+      begin
+        filename = "#{Rails.root}/config/amazon_s3.yml"
+        file = File.open(filename)
+        config = YAML.load(file)[Rails.env]
+
+        if config == nil
+          raise "Could not load config options for #{Rails.env} from #{filename}."
+        end
+
+        @@access_key_id     = ENV['AMAZON_ACCESS_KEY_ID']        || config['access_key_id']
+        @@secret_access_key = ENV['AMAZON_SECRET_ACCESS_KEY']    || config['secret_access_key']
+        @@bucket            = ENV['AMAZON_S3_SWF_UPLOAD_BUCKET'] || config['bucket']
+        @@max_file_size     = ENV['AMAZON_S3_SWF_MAX_FILE_SIZE'] || config['max_file_size']
+        @@acl               = ENV['AMAZON_S3_SWF_UPLOAD_ACL']    || config['acl'] || 'private'
+
         unless @@access_key_id && @@secret_access_key && @@bucket
-          raise "Please configure your environmental variables to include AMAZON_ACCESS_KEY_ID, AMAZON_SECRET_ACCESS_KEY, and AMAZON_S3_SWF_UPLOAD_BUCKET."
+          raise "Please configure your S3 settings in #{filename} or in environment variables before continuing so that S3 SWF Upload can function properly."
         end
-      else
-        begin
-          filename = "#{Rails.root}/config/amazon_s3.yml"
-          file = File.open(filename)
-          config = YAML.load(file)[Rails.env]
-
-          if config == nil
-            raise "Could not load config options for #{Rails.env} from #{filename}."
-          end
-
-          @@access_key_id     = config['access_key_id']
-          @@secret_access_key = config['secret_access_key']
-          @@bucket            = config['bucket']
-          @@max_file_size     = config['max_file_size']
-          @@acl               = config['acl'] || 'private'
-
-
-
-          unless @@access_key_id && @@secret_access_key && @@bucket
-            raise "Please configure your S3 settings in #{filename} before continuing so that S3 SWF Upload can function properly."
-          end
-        rescue Errno::ENOENT
-           # No config file yet. Not a big deal. Just issue a warning
-           puts "WARNING: You are using the S3 SWF Uploader gem, which wants a config file at #{filename}, " +
-              "but none could be found. You should try running 'rails generate s3_swf_upload:uploader'"
-        end
+      rescue Errno::ENOENT
+         # No config file yet. Not a big deal. Just issue a warning
+         puts "WARNING: You are using the S3 SWF Uploader gem, which wants a config file at #{filename}, " +
+            "but none could be found. You should try running 'rails generate s3_swf_upload:uploader'"
       end
     end
   end
