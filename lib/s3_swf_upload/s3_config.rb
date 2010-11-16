4b3fd4f6837c9312b4ddcc38af0d9797d40b26dd
Abner Qian <LargeBagel@gmail.com>
2010-11-15 21:42:51 -0800

    Wow I'm stupid.

--- lib/s3_swf_upload/s3_config.rb
+++ lib/s3_swf_upload/s3_config.rb
@@ -11,19 +11,28 @@ module S3SwfUpload
         file = File.open(filename)
         config = YAML.load(file)[Rails.env]
 
-        @@access_key_id     = ENV['AMAZON_ACCESS_KEY_ID']        || config['access_key_id']
-        @@secret_access_key = ENV['AMAZON_SECRET_ACCESS_KEY']    || config['secret_access_key']
-        @@bucket            = ENV['AMAZON_S3_SWF_UPLOAD_BUCKET'] || config['bucket']
-        @@max_file_size     = ENV['AMAZON_S3_SWF_MAX_FILE_SIZE'] || config['max_file_size']
-        @@acl               = ENV['AMAZON_S3_SWF_UPLOAD_ACL']    || config['acl'] || 'private'
+        @@access_key_id     = config['access_key_id']
+        @@secret_access_key = config['secret_access_key']
+        @@bucket            = config['bucket']
+        @@max_file_size     = config['max_file_size']
+        @@acl               = config['acl'] || 'private'
 
         unless @@access_key_id && @@secret_access_key && @@bucket
-          raise "Please configure your S3 settings in #{filename} or in environment variables before continuing so that S3 SWF Upload can function properly."
+          raise "Please configure your S3 settings in #{filename} before continuing so that S3 SWF Upload can function properly."
         end
       rescue Errno::ENOENT
-         # No config file yet. Not a big deal. Just issue a warning
-         puts "WARNING: You are using the S3 SWF Uploader gem, which wants a config file at #{filename}, " +
-            "but none could be found. You should try running 'rails generate s3_swf_upload:uploader'"
+        # No config file yet. Not a big deal. Just issue a warning
+        puts "WARNING: You are using the S3 SWF Uploader gem, which wants a config file at #{filename}, " +
+          "but none could be found. Now using You should try running 'rails generate s3_swf_upload:uploader'"
+        @@access_key_id     = ENV['AMAZON_ACCESS_KEY_ID']
+        @@secret_access_key = ENV['AMAZON_SECRET_ACCESS_KEY']
+        @@bucket            = ENV['AMAZON_S3_SWF_UPLOAD_BUCKET']
+        @@max_file_size     = ENV['AMAZON_S3_SWF_MAX_FILE_SIZE']
+        @@acl               = ENV['AMAZON_S3_SWF_UPLOAD_ACL']
+        unless @@access_key_id && @@secret_access_key && @@bucket
+          raise "Please configure your S3 settings in environment variables or running 'rails generate s3_swf_upload:uploader' " +
+            "before continuing so that S3 SWF Upload can function properly."
+        end
       end
     end
   end
